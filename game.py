c6f4cd28128faa2521406f96a8a7c0732a410fb5
your name <ivan.omurbekov@yandex.ru>
2025-04-28 12:06:21 +0300

    Commit2804

---
+++ game.py
@@ -0,0 +1,23 @@
+#import sys
+import pygame
+
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+
+def run_game():
+# Инициализирует игру и создает объект экрана.
+    pygame.init()
+    # Назначение цвета фона.
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+    # Создание корабля.
+    ship = Ship(screen)
+
+# Запуск основного цикла игры.
+    while True:
+        gf.check_events(ship)
+        ship.update
+        gf.update_screen(ai_settings, screen, ship)
+run_game()
